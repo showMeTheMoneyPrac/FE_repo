f86256b96b52dede2ba739cbd64e07081676e236
chanyDev <chanyd.dev@gmail.com>
2022-05-19 19:21:31 +0900

    style : 유저 페이지 기본 레이아웃 구현

---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,13 @@
+import UserTemplate from 'components/user/UserTemplate';
+import React from 'react';
+
+const UserPage = () => {
+  return (
+    <>
+      {/* 헤더 영역 */}
+      <UserTemplate />
+    </>
+  );
+};
+
+export default UserPage;
