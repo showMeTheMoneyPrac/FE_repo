080bc9425b822f0c78ee1bde115674dacd45cc65
chanyDev <chanyd.dev@gmail.com>
2022-05-19 19:20:38 +0900

    feat : 유저 페이지 라우팅 처리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import PurchaseList from 'components/user/PurchaseList';
+import UserInfo from 'components/user/UserInfo';
+import UserPage from 'pages/UserPage';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/user" element={<UserPage />}>
+          <Route path="info" element={<UserInfo />} />
+          <Route path="purchase" element={<PurchaseList />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
