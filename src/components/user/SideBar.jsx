21203571a80f5fdef9fdd2b0366f499a411705cc
chanyDev <chanyd.dev@gmail.com>
2022-05-20 18:59:57 +0900

    style : 사이드 바 UI 구현

--- src/components/user/SideBar.jsx
+++ src/components/user/SideBar.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 const SideBar = () => {
+  const { pathname } = useLocation();
+
   return (
     <SideNav>
-      <h2>마이 페이지</h2>
+      <h2 className="nav-title">마이 페이지</h2>
       <ul className="nav-list">
         <li>
-          <Link className="info-link" to="/user/info">
+          <StyledLink
+            $current={pathname !== '/user/purchase'}
+            className="info-link"
+            to="/user/info"
+          >
             내 정보
-          </Link>
+          </StyledLink>
         </li>
         <li>
-          <Link className="purchase-link" to="/user/purchase">
+          <StyledLink
+            $current={pathname === '/user/purchase'}
+            className="purchase-link"
+            to="/user/purchase"
+          >
             구매 목록
-          </Link>
+          </StyledLink>
         </li>
       </ul>
     </SideNav>
@@ -23,16 +33,24 @@ const SideBar = () => {
 };
 
 const SideNav = styled.nav`
-  background-color: pink;
+  background-color: #fff;
+  padding: 3rem 3rem 0 3rem;
+  .nav-title {
+    font-size: 2rem;
+    font-weight: bold;
+  }
   .nav-list {
-    margin-top: 2rem;
+    margin-top: 4rem;
     display: flex;
     flex-direction: column;
-    gap: 2rem;
-    a {
-      display: block;
-    }
+    gap: 4rem;
   }
+`;
+
+const StyledLink = styled(Link)`
+  display: block;
+  font-size: 1.5rem;
+  font-weight: ${(props) => (props.$current ? 'bold' : 'normal')};
 `;
 
 export default SideBar;
