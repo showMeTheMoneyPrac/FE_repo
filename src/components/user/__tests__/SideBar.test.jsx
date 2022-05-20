4bb25d9939190a0fae9092c353009b0b3b1d28f1
chanyDev <chanyd.dev@gmail.com>
2022-05-20 18:57:43 +0900

    test : 네비게이션 메뉴 스타일 변경 테스트 코드 작성

--- src/components/user/__tests__/SideBar.test.jsx
+++ src/components/user/__tests__/SideBar.test.jsx
@@ -1,33 +1,54 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import 'jest-styled-components';
 
 import SideBar from '../SideBar';
 
 describe('<SideBar />', () => {
   const setup = () => {
-    return render(<SideBar />, { wrapper: MemoryRouter });
+    render(<SideBar />, { wrapper: MemoryRouter });
+    const infoLink = screen.getByRole('link', { name: '내 정보' });
+    const purchaseLink = screen.getByRole('link', { name: '구매 목록' });
+
+    return {
+      infoLink,
+      purchaseLink,
+    };
   };
 
   test('컴포넌트를 정확히 렌더링하는지 확인', () => {
-    setup();
+    const { infoLink, purchaseLink } = setup();
 
-    expect(screen.getByRole('link', { name: '내 정보' })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: '구매 목록' })).toBeInTheDocument();
+    expect(infoLink).toBeInTheDocument();
+    expect(purchaseLink).toBeInTheDocument();
   });
 
   test('라우팅 확인', () => {
-    setup();
+    const { infoLink, purchaseLink } = setup();
     const history = createBrowserHistory();
 
-    const infoLink = screen.getByRole('link', { name: '내 정보' });
     fireEvent.click(infoLink);
     history.push('/user/info');
     expect(history.location.pathname).toBe('/user/info');
 
-    const purchaseLink = screen.getByRole('link', { name: '구매 목록' });
     fireEvent.click(purchaseLink);
     history.push('/user/purchase');
     expect(history.location.pathname).toBe('/user/purchase');
+  });
+
+  test('네비게이션 메뉴 스타일 변경 확인', () => {
+    const { infoLink, purchaseLink } = setup();
+
+    expect(infoLink).toHaveStyleRule('font-weight', 'bold');
+    expect(purchaseLink).not.toHaveStyleRule('font-weight', 'bold');
+
+    fireEvent.click(purchaseLink);
+    expect(infoLink).not.toHaveStyleRule('font-weight', 'bold');
+    expect(purchaseLink).toHaveStyleRule('font-weight', 'bold');
+
+    fireEvent.click(infoLink);
+    expect(infoLink).toHaveStyleRule('font-weight', 'bold');
+    expect(purchaseLink).not.toHaveStyleRule('font-weight', 'bold');
   });
 });
