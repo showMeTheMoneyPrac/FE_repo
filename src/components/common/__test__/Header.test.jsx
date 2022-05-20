c6722af75f5f50d99d0c11b8b6f30dc1ce22bae2
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-05-20 14:17:13 +0900

    test :헤더 렌더링 테스트 수정

--- src/components/common/__test__/Header.test.jsx
+++ src/components/common/__test__/Header.test.jsx
@@ -1,14 +1,21 @@
 import { render, screen } from '@testing-library/react';
 
 import Header from 'components/common/Header';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('<Header/>', () => {
+  const setup = () => {
+    return render(<Header />, { wrapper: MemoryRouter });
+  };
+
   test('헤더 렌더링 테스트', () => {
-    render(<Header />);
-    screen.getByText('C#');
-    screen.getByText('로그인');
-    screen.getByText('마이페이지');
-    screen.getByText('장바구니');
-    screen.getByPlaceholderText('상품명을 입력하세요.');
+    setup();
+
+    expect(screen.getByRole('link', { name: 'C#' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '로그인' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', { name: '마이페이지' }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: '장바구니' })).toBeInTheDocument();
   });
 });
