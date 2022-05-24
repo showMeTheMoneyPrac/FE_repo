686378c5ee2784dcdf355d2092fd2f840f04c911
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-05-24 22:36:20 +0900

    chore : 리덕스 스토어 연결

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 
 import GlobalStyle from 'styles/GlobalStyle';
 import App from './App';
+import store from 'redux/configStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+      <GlobalStyle />
+    </Provider>
   </React.StrictMode>,
 );
 
