75142dc7a0b61929a6425c3f81c8d152f6a887de
chanyDev <chanyd.dev@gmail.com>
2022-05-25 15:03:58 +0900

    feat : 유저 정보 토글 커스텀 훅 구현

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,19 @@
+import { useCallback, useState } from 'react';
+
+const useToggle = () => {
+  const [infoToggleState, setInfoToggleState] = useState({
+    nickname: false,
+    cash: false,
+  });
+
+  const handleToggleState = useCallback((e) => {
+    setInfoToggleState((prev) => ({
+      ...prev,
+      [e.target.name]: !prev[e.target.name],
+    }));
+  }, []);
+
+  return { infoToggleState, handleToggleState };
+};
+
+export default useToggle;
