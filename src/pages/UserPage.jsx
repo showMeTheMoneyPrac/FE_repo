9ccbce5b95f023c1d78c94fc4fa79c4af8c9d55e
chanyDev <chanyd.dev@gmail.com>
2022-05-25 23:20:31 +0900

    feat : 로그인 하지 않은 사용자에 대해 리다이렉트 처리

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 import Header from 'components/common/Header';
 import UserTemplate from 'components/user/UserTemplate';
 
 const UserPage = () => {
+  const isLoggedIn = localStorage.getItem('nickname');
+
+  if (!isLoggedIn) return <Navigate replace to="/" />;
+
   return (
     <>
       <Header />
