b21ce7db8582b39ba37e9f9b71c8523dcb546b28
chanyDev <chanyd.dev@gmail.com>
2022-05-25 15:05:18 +0900

    feat : 닉네임 변경, 포인트 충전 UI 구현

--- src/components/user/UserInfo.jsx
+++ src/components/user/UserInfo.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { fetchUserInfo } from 'api/user';
+import useToggle from 'hooks/useToggle';
 
 const UserInfo = () => {
   const [userInfo, setUserInfo] = useState(null);
+  const { infoToggleState, handleToggleState } = useToggle();
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -18,21 +20,63 @@ const UserInfo = () => {
   return (
     <UserInfoWrapper>
       <h3 className="user-info-title">내 정보</h3>
-      <div className="info-container">
+      <InfoWrapper $isActive={infoToggleState.nickname !== true}>
         <h4 className="info-title">닉네임</h4>
         <p className="info-content">{userInfo?.nickname}</p>
-        <button className="info-btn">닉네임 수정</button>
-      </div>
-      <div className="info-container">
+        {!infoToggleState.nickname ? (
+          <button
+            onClick={handleToggleState}
+            name="nickname"
+            className="info-btn"
+          >
+            닉네임 변경
+          </button>
+        ) : (
+          <InfoChangeForm>
+            <label htmlFor="userNickname">새로운 닉네임</label>
+            <input id="userNickname" type="text" />
+            <ActionBtnWrapper>
+              <button className="submit-btn">저장</button>
+              <button
+                onClick={handleToggleState}
+                name="nickname"
+                className="cancel-btn"
+              >
+                취소
+              </button>
+            </ActionBtnWrapper>
+          </InfoChangeForm>
+        )}
+      </InfoWrapper>
+      <InfoWrapper $isActive={infoToggleState.cash !== true}>
         <h4 className="info-title">잔여 포인트</h4>
         <p className="info-content">{userInfo?.cash}</p>
-        <button className="info-btn">포인트 충전</button>
-      </div>
-      <div className="info-container">
+        {!infoToggleState.cash ? (
+          <button onClick={handleToggleState} name="cash" className="info-btn">
+            포인트 충전
+          </button>
+        ) : (
+          <InfoChangeForm>
+            <label htmlFor="userCash">충전할 포인트</label>
+            <input id="userCash" type="text" />
+            <ActionBtnWrapper>
+              <button className="submit-btn">저장</button>
+              <button
+                onClick={handleToggleState}
+                name="cash"
+                className="cancel-btn"
+              >
+                취소
+              </button>
+            </ActionBtnWrapper>
+          </InfoChangeForm>
+        )}
+      </InfoWrapper>
+      <InfoWrapper $isActive={true}>
         <h4 className="info-title">주소</h4>
         <p className="info-content">{userInfo?.address}</p>
-        <button className="info-btn">주소 수정</button>
-      </div>
+        <button className="info-btn">주소 변경</button>
+      </InfoWrapper>
     </UserInfoWrapper>
   );
 };
@@ -43,7 +87,7 @@ const UserInfoWrapper = styled.div`
   flex: 1;
   padding: 6rem;
   .user-info-title {
-    font-size: 2.4rem;
+    font-size: 2.2rem;
     font-weight: bold;
     padding-bottom: 2rem;
     margin-bottom: 2rem;
@@ -53,6 +97,11 @@ const UserInfoWrapper = styled.div`
     padding: 2rem 0;
     border-bottom: 1px solid #dedede;
   }
+`;
+
+const InfoWrapper = styled.div`
+  padding: 2rem 0;
+  border-bottom: ${(props) => props.$isActive && '1px solid #dedede'};
   .info-title {
     margin-bottom: 1rem;
     font-size: 1.4rem;
@@ -67,7 +116,45 @@ const UserInfoWrapper = styled.div`
     background-color: #222;
     font-size: 1.4rem;
     color: #fff;
-    padding: 0.6rem 1.2rem;
+    padding: 0.8rem 1.5rem;
+    border-radius: 0.8rem;
+  }
+`;
+
+const InfoChangeForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  > label {
+    font-size: 1.4rem;
+    color: #8e8e8e;
+    margin-bottom: 1rem;
+  }
+  > input {
+    padding-bottom: 1rem;
+    border-bottom: 1px solid #dedede;
+    &:focus {
+      border-bottom: 1px solid #000;
+    }
+  }
+`;
+
+const ActionBtnWrapper = styled.div`
+  display: flex;
+  gap: 3rem;
+  justify-content: center;
+  margin-top: 2rem;
+  > .cancel-btn {
+    background-color: #dedede;
+    font-size: 1.4rem;
+    color: #fff;
+    padding: 0.8rem 1.5rem;
+    border-radius: 0.8rem;
+  }
+  .submit-btn {
+    background-color: #222;
+    font-size: 1.4rem;
+    color: #fff;
+    padding: 0.8rem 1.5rem;
     border-radius: 0.8rem;
   }
 `;
