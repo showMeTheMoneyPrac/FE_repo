c3f061a967204a55f5d86610122f637b42245a14
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-05-25 13:39:57 +0900

    remove : 불필요 코드 제거

--- src/components/form/RegistForm.jsx
+++ src/components/form/RegistForm.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 import LabelInput from 'components/form/LabelInput';
-import useSearchAddress from 'hooks/useSearchAddress';
 
 const RegistForm = () => {
   return (
@@ -11,38 +10,37 @@ const RegistForm = () => {
         inputId="memberId"
         inputType="text"
         labelText="아이디"
-        InputPlaceholder="이메일 형식의 아이디를 입력하세요."
+        inputPlaceholder="이메일 형식의 아이디를 입력하세요."
       />
       <LabelInput
         inputId="memberNickname"
         inputType="text"
         labelText="닉네임"
-        InputPlaceholder="닉네임을 입력하세요."
+        inputPlaceholder="닉네임을 입력하세요."
       />
       <LabelInput
         inputId="memberPw"
         inputType="password"
         labelText="비밀번호"
-        InputPlaceholder="비밀번호를 입력하세요."
+        inputPlaceholder="비밀번호를 입력하세요."
       />
       <LabelInput
         inputId="memberPwCheck"
         inputType="password"
         labelText="비밀번호 확인"
-        InputPlaceholder="비밀번호를 한번더 입력해주세요."
+        inputPlaceholder="비밀번호를 한번더 입력해주세요."
       />
       <SearchAddressWrapper>
         <LabelInput
           inputId="memberAddress"
           inputType="text"
           labelText="주소"
-          InputPlaceholder="주소를 검색해주세요"
+          inputPlaceholder="주소를 검색해주세요"
           readOnly={true}
         />
         <input
           className="search-address-btn"
           type="button"
-          onClick={useSearchAddress}
           value="주소 검색"
         ></input>
       </SearchAddressWrapper>
@@ -50,7 +48,7 @@ const RegistForm = () => {
         inputId="memberDetailAddress"
         inputType="text"
         labelText="상세 주소"
-        InputPlaceholder="상세주소를 입력해주세요."
+        inputPlaceholder="상세주소를 입력해주세요."
       />
       <button className="submit-btn">회원가입</button>
     </RegistFormWrapper>
