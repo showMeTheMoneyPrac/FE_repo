c2726366e0b40b3ac86364a9b8c6ab0fe7348d9b
chanyDev <chanyd.dev@gmail.com>
2022-05-26 16:43:53 +0900

    feat : 구매 목록 요청 구현 및 UI 완성

--- src/components/user/PurchaseList.jsx
+++ src/components/user/PurchaseList.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import PurchaseItem from 'components/user/PurchaseItem';
+
+import PurchaseDetail from 'components/user/PurchaseDetail';
+import { fetchPurchaseList } from 'api/product';
 
 const PurchaseList = () => {
+  const [purchaseList, setPurchaseList] = useState(null);
+
+  useEffect(() => {
+    const handleFetchPurchaseList = async () => {
+      const { data } = await fetchPurchaseList();
+      setPurchaseList(data.ordersList);
+    };
+
+    handleFetchPurchaseList();
+  }, []);
+
   return (
     <PurchaseListWrapper>
       <h3 className="purchase-title">구매 목록</h3>
       <ul className="purchase-list">
-        <PurchaseItem />
-        <PurchaseItem />
-        <PurchaseItem />
+        {purchaseList?.map((purchaseItem) => (
+          <PurchaseItem key={purchaseItem.ordersId}>
+            {purchaseItem.ordersDetailList.map((detail) => (
+              <PurchaseDetail key={detail.orderDetailId} detail={detail} />
+            ))}
+            <RefundButtonWrapper>
+              <button className="refund-btn">선택 상품 환불</button>
+            </RefundButtonWrapper>
+          </PurchaseItem>
+        ))}
       </ul>
     </PurchaseListWrapper>
   );
@@ -30,7 +50,29 @@ const PurchaseListWrapper = styled.div`
   .purchase-list {
     display: flex;
     flex-direction: column;
-    gap: 2rem;
+    gap: 3rem;
+  }
+`;
+
+const PurchaseItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  padding: 2rem;
+  border-radius: 2rem;
+  box-shadow: 2px 2px 10px 2px rgba(0, 0, 0, 0.1);
+`;
+
+const RefundButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  .refund-btn {
+    font-size: 1.4rem;
+    background-color: #cecece;
+    color: #fff;
+    padding: 0.8rem 1.5rem;
+    border-radius: 0.8rem;
   }
 `;
 
