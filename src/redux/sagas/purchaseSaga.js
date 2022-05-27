e17b77d5a8ffe68afce1c6d387c7f1f5fdd095b6
chanyDev <chanyd.dev@gmail.com>
2022-05-27 18:09:57 +0900

    feat : 리뷰 수정 사가 구현

--- src/redux/sagas/purchaseSaga.js
+++ src/redux/sagas/purchaseSaga.js
@@ -1,12 +1,36 @@
-import { takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 
 import * as purchaseAPI from 'api/purchse';
-import { initPurchaseList } from 'redux/modules/purchase';
+import {
+  initPurchaseList,
+  initPurchaseListSuccess,
+  updateReview,
+  updateReviewSuccess,
+} from 'redux/modules/purchase';
+import { closeModal } from 'redux/modules/modal';
 
-function* initPurchaseListSaga(action) {}
+function* initPurchaseListSaga(action) {
+  const { data } = yield call(purchaseAPI.fetchPurchaseListAPI);
+  yield put(initPurchaseListSuccess(data));
+}
+
+function* updateReviewSaga(action) {
+  const { id, reviewTitle, content } = action.payload;
+
+  const { data } = yield call(() =>
+    purchaseAPI.updateReviewAPI(id, {
+      reviewTitle,
+      content,
+    }),
+  );
+
+  yield put(updateReviewSuccess(data));
+  yield put(closeModal());
+}
 
 function* purchaseSaga() {
   yield takeLatest(initPurchaseList, initPurchaseListSaga);
+  yield takeLatest(updateReview, updateReviewSaga);
 }
 
 export default purchaseSaga;
