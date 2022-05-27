251c5fa20b0335bb660c33a5d45998f7ef7e811b
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-05-27 18:07:38 +0900

    feat : 로그인 회원가입 예외처리 구현

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 
 import LabelInput from 'components/form/LabelInput';
-import { loginUser } from 'api/user';
 import { closeModal } from 'redux/modules/modal';
 import { initializeForm } from 'redux/modules/form';
+import { loginUserAPI } from 'api/user';
+import { userLogin } from 'redux/modules/user';
 
 const LoginForm = () => {
   const dispatch = useDispatch();
+  const [errorMsg, setErrorMsg] = useState('');
   const loginForm = useSelector(({ form }) => form.loginForm);
+  const { email, password } = loginForm;
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    if (!email) {
+      setErrorMsg('아이디를 입력해주세요');
+      return;
+    }
+
+    if (!password) {
+      setErrorMsg('비밀번호를 입력해주세요');
+      return;
+    }
+
     const loginPayload = {
-      email: loginForm.email,
-      password: loginForm.password,
+      email,
+      password,
     };
 
-    const res = await loginUser(loginPayload);
+    const { data } = await loginUserAPI(loginPayload);
 
-    localStorage.setItem('nickname', res.data.nickname);
+    localStorage.setItem('nickname', data.nickname);
+    dispatch(userLogin());
     dispatch(closeModal());
     dispatch(initializeForm());
   };
@@ -42,6 +56,7 @@ const LoginForm = () => {
         form="loginForm"
       />
       <button className="submit-btn">로그인</button>
+      <ErrorMsg>{errorMsg}</ErrorMsg>
     </LoginFormWrapper>
   );
 };
@@ -49,8 +64,8 @@ const LoginForm = () => {
 const LoginFormWrapper = styled.form`
   display: flex;
   flex-direction: column;
-  padding: 2rem 0;
-  gap: 2rem;
+  padding-top: 2rem;
+  gap: 1.5rem;
   .submit-btn {
     background-color: #000;
     color: #fff;
@@ -58,6 +73,12 @@ const LoginFormWrapper = styled.form`
     border-radius: 1rem;
     padding: 1rem;
   }
+`;
+
+const ErrorMsg = styled.p`
+  font-size: 1.4rem;
+  color: #ff0000;
+  height: 1.4rem;
 `;
 
 export default LoginForm;
