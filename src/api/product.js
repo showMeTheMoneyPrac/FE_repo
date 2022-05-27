e7b39ff9a8761d62fc3316dbbc0a34820c126bbf
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-05-27 18:06:51 +0900

    refactor : 변수명 변경

--- src/api/product.js
+++ src/api/product.js
@@ -1,11 +1,12 @@
 import client from 'api/index';
 
-export const fetchProductList = (payload) => {
-  return client.get(
-    `/products/search?sort=${payload.sort ? payload.sort : ''}&category=${
-      payload.category ? payload.category : ''
-    }&searchKeyword=${
-      payload.searchKeyword ? payload.searchKeyword : ''
-    }&page=${payload.page}`,
-  );
+export const fetchProductListAPI = (payload) => {
+  return client.get(`/products/search`, {
+    params: {
+      sort: `${payload.sort ? payload.sort : ''}`,
+      category: `${payload.category ? payload.category : ''}`,
+      searchKeyword: `${payload.searchKeyword ? payload.searchKeyword : ''}`,
+      page: 0,
+    },
+  });
 };
