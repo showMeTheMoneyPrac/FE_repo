d85fd209806e8c5dc65f15a8dc3d2bb46f536066
chanyDev <chanyd.dev@gmail.com>
2022-05-27 14:28:29 +0900

    feat : 리덕스 사가 보일러 플레이트 구현

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,16 +1,24 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 
 import modal from 'redux/modules/modal';
 import form from 'redux/modules/form';
+import rootSaga from 'redux/sagas';
 
 const reducer = combineReducers({
   modal,
   form,
 });
 
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
+
 const store = configureStore({
   reducer,
+  middleware,
   devTools: true,
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
