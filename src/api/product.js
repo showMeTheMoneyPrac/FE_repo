14f064565df960e691f2c28380c3e2187ec15d28
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-05-27 14:37:41 +0900

    remove : 중복 코드 제거

--- src/api/product.js
+++ src/api/product.js
@@ -9,13 +9,3 @@ export const fetchProductList = (payload) => {
     }&page=${payload.page}`,
   );
 };
-
-export const fetchProductList = (payload) => {
-  return client.get(
-    `/products/search?sort=${payload.sort ? payload.sort : ''}&category=${
-      payload.category ? payload.category : ''
-    }&searchKeyword=${
-      payload.searchKeyword ? payload.searchKeyword : ''
-    }&page=${payload.page}`,
-  );
-};
