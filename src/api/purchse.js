59471883b917e7f0f7382fa260d72cac52cc9a85
chanyDev <chanyd.dev@gmail.com>
2022-05-27 18:08:44 +0900

    feat : 리뷰 업데이트 api 구현

--- src/api/purchse.js
+++ src/api/purchse.js
@@ -1,7 +1,15 @@
 import client from 'api/index';
 
-export const fetchPurchaseList = () => {
+export const fetchPurchaseListAPI = () => {
   return client.get('/orders', {
+    headers: {
+      Authorization: localStorage.getItem('nickname'),
+    },
+  });
+};
+
+export const updateReviewAPI = (reviewId, payload) => {
+  return client.put(`/reviews/${reviewId}`, payload, {
     headers: {
       Authorization: localStorage.getItem('nickname'),
     },
