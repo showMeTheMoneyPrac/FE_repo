5a4aad6eef16cb7c12c2f6acb35504c7dd8d9619
chanyDev <chanyd.dev@gmail.com>
2022-05-27 18:09:47 +0900

    feat : 구매 목록 전역 상태로 관리

--- src/redux/modules/purchase.js
+++ src/redux/modules/purchase.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, current } from '@reduxjs/toolkit';
 
 const initialState = {
   purchaseList: null,
@@ -8,8 +8,38 @@ const purchaseSlice = createSlice({
   name: 'purchase',
   initialState,
   reducers: {
-    initPurchaseList(state, action) {},
+    initPurchaseList: () => initialState,
+    initPurchaseListSuccess(state, { payload }) {
+      state.purchaseList = payload.ordersList;
+    },
+    updateReview() {},
+    updateReviewSuccess(state, { payload }) {
+      state.purchaseList = state.purchaseList.map((purchaseItem) => {
+        const newOrderDetailList = purchaseItem.ordersDetailList.map(
+          (orderDetail) => {
+            return orderDetail.review?.id !== payload.reviewId
+              ? orderDetail
+              : {
+                  ...orderDetail,
+                  review: {
+                    ...orderDetail.review,
+                    id: payload.reviewId,
+                    title: payload.reviewTitle,
+                    content: payload.content,
+                  },
+                };
+          },
+        );
+        return { ...purchaseItem, ordersDetailList: newOrderDetailList };
+      });
+    },
   },
 });
 
-export const { initPurchaseList } = purchaseSlice.actions;
+export const {
+  initPurchaseList,
+  initPurchaseListSuccess,
+  updateReview,
+  updateReviewSuccess,
+} = purchaseSlice.actions;
+export default purchaseSlice.reducer;
