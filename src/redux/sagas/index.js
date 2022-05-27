c9907cf564566ede7ae7f832d5934b21dbde8795
chanyDev <chanyd.dev@gmail.com>
2022-05-27 15:05:49 +0900

    refactor : 루트 사가 수정

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
+
 import purchaseSaga from 'redux/sagas/purchaseSaga';
 
 export default function* rootSaga() {
-  yield all([purchaseSaga]);
+  yield all([purchaseSaga()]);
 }
