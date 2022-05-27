1192352a9b32389eb8713c0fa95a82b46d890ece
chanyDev <chanyd.dev@gmail.com>
2022-05-27 18:09:26 +0900

    refactor : 커스텀 훅 상태 이름 변경

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 const useInput = (initialState = null) => {
-  const [changeInfo, setChangeInfo] = useState(initialState);
+  const [inputValue, setInputValue] = useState(initialState);
 
-  const handleChangeInfo = (e) => {
-    setChangeInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  const handleChangeInput = (e) => {
+    setInputValue((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  return { changeInfo, setChangeInfo, handleChangeInfo };
+  return { inputValue, setInputValue, handleChangeInput };
 };
 
 export default useInput;
