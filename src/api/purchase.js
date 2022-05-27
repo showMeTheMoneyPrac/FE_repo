727ed9f8b0f470cdbee5db319d7f5838adb7a6f2
chanyDev <chanyd.dev@gmail.com>
2022-05-27 18:48:08 +0900

    refactor : 파일명 수정

--- src/api/purchase.js
+++ src/api/purchase.js
@@ -8,8 +8,24 @@ export const fetchPurchaseListAPI = () => {
   });
 };
 
+export const createReviewAPI = (orderDetailId, payload) => {
+  return client.post(`/reviews/${orderDetailId}`, payload, {
+    headers: {
+      Authorization: localStorage.getItem('nickname'),
+    },
+  });
+};
+
 export const updateReviewAPI = (reviewId, payload) => {
   return client.put(`/reviews/${reviewId}`, payload, {
+    headers: {
+      Authorization: localStorage.getItem('nickname'),
+    },
+  });
+};
+
+export const deleteReviewAPI = (reviewId) => {
+  return client.delete(`/reviews/${reviewId}`, {
     headers: {
       Authorization: localStorage.getItem('nickname'),
     },
