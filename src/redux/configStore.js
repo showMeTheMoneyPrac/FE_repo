99e4a33edd621ee1323c52791d8fad7c785bcd96
chanyDev <chanyd.dev@gmail.com>
2022-05-30 17:45:14 +0900

    feat : 장바구니 리듀서 추가

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -5,12 +5,14 @@ import modal from 'redux/modules/modal';
 import form from 'redux/modules/form';
 import purchase from 'redux/modules/purchase';
 import user from 'redux/modules/user';
+import bucket from 'redux/modules/bucket';
 import rootSaga from 'redux/sagas';
 
 const reducer = combineReducers({
   modal,
   form,
   purchase,
+  bucket,
   user,
 });
 
