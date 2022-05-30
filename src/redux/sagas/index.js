d90f7f32c56126369d56617bbef160017c1a99cf
chanyDev <chanyd.dev@gmail.com>
2022-05-30 17:45:03 +0900

    feat : 장바구니 saga 추가

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 
 import purchaseSaga from 'redux/sagas/purchaseSaga';
+import bucketSaga from 'redux/sagas/bucketSaga';
 
 export default function* rootSaga() {
-  yield all([purchaseSaga()]);
+  yield all([purchaseSaga(), bucketSaga()]);
 }
