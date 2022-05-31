ee41eb7706d391a806a67e7314c0be20f7066bb6
chanyDev <chanyd.dev@gmail.com>
2022-05-31 19:24:53 +0900

    feat : 로그인 하지 않은 사용자에 대한 리다이렉트 처리

--- src/pages/BucketPage.jsx
+++ src/pages/BucketPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 import Header from 'components/common/Header';
 import BucketTemplate from 'components/bucket/BucketTemplate';
 
 const BucketPage = () => {
+  const isLoggedIn = localStorage.getItem('nickname');
+
+  if (!isLoggedIn) return <Navigate replace to="/" />;
   return (
     <>
       <Header />
