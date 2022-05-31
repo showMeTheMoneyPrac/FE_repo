7413caac97104fb30e72dbda20f697f5ca788a33
chanyDev <chanyd.dev@gmail.com>
2022-05-31 16:32:28 +0900

    feat : 상품 환불 구현

--- src/components/user/PurchaseDetail.jsx
+++ src/components/user/PurchaseDetail.jsx
@@ -1,15 +1,16 @@
 import React, { useCallback } from 'react';
 import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import StyledCheckbox from 'components/common/StyledCheckbox';
-import useSelect from 'hooks/useSelect';
 import { openReviewModal } from 'redux/modules/modal';
 import PurchaseReview from 'components/user/PurchaseReview';
+import { selectPurchaseItem } from 'redux/modules/purchase';
 
 const PurchaseDetail = ({ detail }) => {
   const dispatch = useDispatch();
-  const { isSelected, handleSelector } = useSelect();
+  const selectedList = useSelector(({ purchase }) => purchase.selectedList);
 
   const handleVisibleModal = useCallback(
     (reviewId) => {
@@ -18,24 +19,45 @@ const PurchaseDetail = ({ detail }) => {
     [dispatch],
   );
 
+  const handleSelectPurchaseItem = (e) => {
+    if (e.target.checked) {
+      dispatch(selectPurchaseItem({ type: false, id: +e.target.id }));
+    } else {
+      dispatch(selectPurchaseItem({ type: true, id: +e.target.id }));
+    }
+  };
+
   return (
     <PurchaseDetailWrapper>
       <label htmlFor={detail.orderDetailId}>
         <input
-          onChange={handleSelector}
+          onChange={handleSelectPurchaseItem}
+          checked={selectedList?.includes(detail.orderDetailId)}
           id={detail.orderDetailId}
           className="a11y-hidden"
           type="checkbox"
         />
-        <StyledCheckbox isSelected={isSelected} />
+        <StyledCheckbox
+          isSelected={selectedList?.includes(detail.orderDetailId)}
+        />
       </label>
       <PurchaseInfo>
-        <img className="product-image" src={detail.firstImg} alt="product" />
-        <ProductInfo>
-          <h4 className="product-title">{detail.title}</h4>
-          <p className="product-option">{detail.ea}ea</p>
-          <p className="product-price">{detail.price.toLocaleString()}원</p>
-        </ProductInfo>
+        <Link to={`/product/${detail.productId}`} className="product-img-link">
+          <img className="product-image" src={detail.firstImg} alt="product" />
+        </Link>
+        <Link
+          to={`/product/${detail.productId}`}
+          className="product-detail-link"
+        >
+          <ProductInfo>
+            <h4 className="product-title">{detail.title}</h4>
+            <div className="product-option">
+              <p>{detail.optionContent}</p>
+              <p>({detail.ea}ea)</p>
+            </div>
+            <p className="product-price">{detail.price.toLocaleString()}원</p>
+          </ProductInfo>
+        </Link>
         {!detail.review && (
           <button
             onClick={() =>
@@ -61,10 +83,13 @@ const PurchaseDetailWrapper = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
-  min-height: 6rem;
-  padding: 0 0 1.5rem 3rem;
+  gap: 2rem;
+  padding: 0 0 2rem 3rem;
   border-bottom: 1px solid #cecece;
+  &:last-child {
+    border-bottom: none;
+    padding: 0 0 0 3rem;
+  }
   > label {
     position: absolute;
     left: -0.5rem;
@@ -76,13 +101,19 @@ const PurchaseDetailWrapper = styled.div`
 `;
 
 const PurchaseInfo = styled.div`
-  flex: 1;
   display: flex;
-  max-height: 6rem;
-  .product-image {
+  .product-img-link {
     width: 6rem;
-    height: 6rem;
+  }
+  .product-image {
+    width: 100%;
+    height: 100%;
     object-fit: cover;
+  }
+  .product-detail-link {
+    display: flex;
+    flex: 1;
+    margin: 0 2rem;
   }
   .review-write-btn {
     align-self: flex-end;
@@ -95,15 +126,15 @@ const PurchaseInfo = styled.div`
 `;
 
 const ProductInfo = styled.div`
-  flex: 1;
   display: flex;
   flex-direction: column;
-  gap: 0.5rem;
-  margin-left: 2rem;
+  gap: 1rem;
   .product-title {
     font-size: 1.6rem;
   }
   .product-option {
+    display: flex;
+    gap: 0.5rem;
     font-size: 1.4rem;
     color: #8e8e8e;
   }
--- src/components/user/PurchaseList.jsx
+++ src/components/user/PurchaseList.jsx
@@ -1,19 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 
 import ReviewModal from 'components/modal/ReviewModal';
 import PurchaseDetail from 'components/user/PurchaseDetail';
-import { initPurchaseList } from 'redux/modules/purchase';
+import {
+  initPurchaseList,
+  selectAllPurchaseItem,
+} from 'redux/modules/purchase';
+import StyledCheckbox from 'components/common/StyledCheckbox';
+import { refundPurchaseAPI } from 'api/purchase';
 
 const PurchaseList = () => {
   const dispatch = useDispatch();
   const purchaseList = useSelector(({ purchase }) => purchase.purchaseList);
+  const selectedList = useSelector(({ purchase }) => purchase.selectedList);
   const { visible } = useSelector(({ modal }) => modal.reviewModal);
+
+  const totalList = useMemo(
+    () =>
+      purchaseList?.reduce(
+        (prev, purchaseItem) => prev + purchaseItem.ordersDetailList.length,
+        0,
+      ),
+    [purchaseList],
+  );
 
   useEffect(() => {
     dispatch(initPurchaseList());
   }, [dispatch]);
+
+  const handleSelectAllPurchaseItem = (e) => {
+    if (e.target.checked) {
+      dispatch(selectAllPurchaseItem(true));
+    } else {
+      dispatch(selectAllPurchaseItem(false));
+    }
+  };
+
+  const handleRefundPurchaseItem = async () => {
+    if (!selectedList.length) return;
+
+    const stringParam = selectedList.join();
+    try {
+      await refundPurchaseAPI(stringParam);
+      dispatch(initPurchaseList());
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <PurchaseListWrapper>
@@ -24,12 +59,29 @@ const PurchaseList = () => {
             {purchaseItem.ordersDetailList.map((detail) => (
               <PurchaseDetail key={detail.orderDetailId} detail={detail} />
             ))}
-            <RefundButtonWrapper>
-              <button className="refund-btn">선택 상품 환불</button>
-            </RefundButtonWrapper>
           </PurchaseItem>
         ))}
       </ul>
+      {purchaseList && (
+        <RefundWrapper>
+          <div className="all-selector">
+            <label htmlFor="allSelector">
+              <input
+                onChange={handleSelectAllPurchaseItem}
+                checked={totalList === selectedList.length}
+                id="allSelector"
+                className="a11y-hidden"
+                type="checkbox"
+              />
+              <StyledCheckbox isSelected={totalList === selectedList.length} />
+            </label>
+            <span className="selector-text">전체 선택</span>
+          </div>
+          <button onClick={handleRefundPurchaseItem} className="refund-btn">
+            선택 상품 환불
+          </button>
+        </RefundWrapper>
+      )}
       {visible && <ReviewModal />}
     </PurchaseListWrapper>
   );
@@ -57,22 +109,40 @@ const PurchaseListWrapper = styled.div`
 const PurchaseItem = styled.li`
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
+  gap: 2rem;
   padding: 2rem;
   border-radius: 2rem;
   box-shadow: 2px 2px 10px 2px rgba(0, 0, 0, 0.1);
 `;
 
-const RefundButtonWrapper = styled.div`
+const RefundWrapper = styled.div`
   display: flex;
-  justify-content: center;
-  margin-top: 1rem;
-  .refund-btn {
+  align-items: center;
+  margin-top: 3rem;
+  padding: 0 2rem;
+  .all-selector {
+    display: flex;
+    align-items: center;
+    &:after {
+      display: block;
+      content: '';
+      width: 1px;
+      height: 1.4rem;
+      border-right: 1px solid #cecece;
+    }
+  }
+  .selector-text {
+    display: block;
     font-size: 1.4rem;
-    background-color: #cecece;
-    color: #fff;
-    padding: 0.8rem 1.5rem;
-    border-radius: 0.8rem;
+    font-weight: bold;
+    padding: 0 2rem;
+  }
+  .refund-btn {
+    all: unset;
+    padding: 0 2rem;
+    font-size: 1.4rem;
+    font-weight: bold;
+    cursor: pointer;
   }
 `;
 
