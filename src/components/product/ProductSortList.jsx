f43090b891532aaafdf945e2c1a79263cab10a70
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-05-31 19:03:05 +0900

    refactor : 게시물 필터링 라우팅 수정

--- src/components/product/ProductSortList.jsx
+++ src/components/product/ProductSortList.jsx
@@ -1,47 +1,68 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import useSearchQuery from 'hooks/useSearchQuery';
+import { useDispatch } from 'react-redux';
+import { initializeProductList } from 'redux/modules/product';
 
 const ProductSortModal = ({ visible }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const category = useSearchQuery('category');
   const search = useSearchQuery('search');
+
+  const handleInitilizePage = (route) => {
+    dispatch(initializeProductList());
+    navigate(route);
+  };
 
   return (
     <ProductSortList visible={visible}>
       <ProductSortItem>
-        <Link
+        <SortBtn
+          onClick={() =>
+            handleInitilizePage(
+              `/product?${category ? `category=${category}&` : ''}${
+                search ? `search=${search}&` : ''
+              }sort=recent`,
+            )
+          }
           className="sort-link"
-          to={`/product?${category ? `category=${category}&` : ''}${
-            search ? `search=${search}&` : ''
-          }sort=recent`}
         >
           <p className="sort-item">최신순</p>
           <p className="sort-description">상품을 최신순으로 정렬</p>
-        </Link>
+        </SortBtn>
       </ProductSortItem>
       <ProductSortItem>
-        <Link
+        <SortBtn
+          onClick={() =>
+            handleInitilizePage(
+              `/product?${category ? `category=${category}&` : ''}${
+                search ? `search=${search}&` : ''
+              }sort=cost`,
+            )
+          }
           className="sort-link"
-          to={`/product?${category ? `category=${category}&` : ''}${
-            search ? `search=${search}&` : ''
-          }sort=cost`}
         >
           <p className="sort-item">가격순</p>
           <p className="sort-description">상품 가격 높은순으로 정렬</p>
-        </Link>
+        </SortBtn>
       </ProductSortItem>
       <ProductSortItem>
-        <Link
+        <SortBtn
+          onClick={() =>
+            handleInitilizePage(
+              `/product?${category ? `category=${category}&` : ''}${
+                search ? `search=${search}&` : ''
+              }sort=review`,
+            )
+          }
           className="sort-link"
-          to={`/product?${category ? `category=${category}&` : ''}${
-            search ? `search=${search}&` : ''
-          }sort=review`}
         >
           <p className="sort-item">리뷰순</p>
           <p className="sort-description">상품의 리뷰가 많은순으로 정렬</p>
-        </Link>
+        </SortBtn>
       </ProductSortItem>
     </ProductSortList>
   );
@@ -60,7 +81,7 @@ const ProductSortList = styled.ul`
 
 const ProductSortItem = styled.li`
   padding: 1rem;
-  .sort-link {
+  /* .sort-link {
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
@@ -73,9 +94,25 @@ const ProductSortItem = styled.li`
       color: #8e8e8e;
       font-size: 1.2rem;
     }
-  }
+  } */
   &:hover {
     background-color: #f1f1f1;
+  }
+`;
+
+const SortBtn = styled.button`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  /* cursor: pointer; */
+  .sort-item {
+    font-size: 1.4rem;
+    font-weight: 500;
+  }
+
+  .sort-description {
+    color: #8e8e8e;
+    font-size: 1.2rem;
   }
 `;
 
